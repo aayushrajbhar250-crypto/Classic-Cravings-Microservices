0c31f0ea5a819a4ce12c8840a2c5bc8f9d88ef61
Aayush Kumar Rajbhar <aayushrajbhar250@gmail.com>
2026-04-22 21:30:44 +0530

    feat: update API Gateway config and add new configuration classes,Security added

---
+++ api-gateway/src/main/java/com/gateway/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private RollConverter rollConverter;
+
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+
+        httpSecurity.cors (e -> e.disable ())
+                .csrf (e -> e.disable())
+                .authorizeExchange (exchange ->
+                        exchange.pathMatchers ( HttpMethod.GET ).permitAll ()
+//                                .pathMatchers ( HttpMethod.POST,"/foods/**" ).hasRole ( "ADMIN" )
+//                                .pathMatchers ( "/restaurant/**" ).hasRole ( "ADMIN" )
+                                .anyExchange ().authenticated()
+                ).oauth2ResourceServer (config ->
+                        config.jwt (
+                                jwt ->
+                                        jwt.jwtAuthenticationConverter (roleExtractor () )
+                        ));
+
+
+        return httpSecurity.build();
+
+    }
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> roleExtractor(){
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter ( rollConverter );
+        return new ReactiveJwtAuthenticationConverterAdapter ( converter );
+    }
+}
